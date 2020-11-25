a66425dc48aa366685d93c1c4e9f54a8f175788c
Lara Fernanda Gastaldi <lara.gastaldi@byseven.com.br>
2020-11-25 17:26:15 -0300

    added regex exercise

---
+++ hackerrank/findsubword.py
@@ -0,0 +1,26 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import re, sys
+inp = sys.stdin.read().split("\n")
+cont = False
+queries = []
+phrases = []
+found = 0
+
+for sentence in inp[1:]:
+    if sentence.isdigit():
+        cont = True
+        continue
+    if cont:
+        queries.append(sentence)
+    else:
+        phrases.append(sentence)
+
+foundlst = []
+
+for querie in queries:
+    for phrase in phrases:
+        found += len(re.findall("[A-Za-z]+"+querie+"[A-Za-z]+",phrase))
+    foundlst.append(found)
+
+for num in foundlst:
+    print(num)
